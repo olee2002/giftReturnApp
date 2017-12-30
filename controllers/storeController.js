6c882ddcf22beed043545aeb4548b950533b8775
babylee2002 <babylee2002@gmail.com>
2017-12-30 12:00:32 -0500

    create new store added

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -3,22 +3,29 @@ const router = express.Router({ mergeParams: true })
 //require database
 const User = require('../db/models/User')
 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     const userId = req.params.userId
     User.findById(userId)
-    .then((user)=>{
-        res.render('stores/index',{
-            userFullName:`${user.firstName}${user.lastName}`,
-            userId:user._id,
-            stores:user.stores,
-            pageTitle:'Stores'
+        .then((user) => {
+            res.render('stores/index', {
+                userFullName: `${user.firstName}${user.lastName}`,
+                userId: user._id,
+                stores: user.stores,
+                pageTitle: 'Stores'
+            })
         })
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
+        .catch((error) => {
+            console.log(error)
+        })
 })
+router.get('/new', (req, res) => {
+    const userId = req.params.userId
+    res.render('stores/new', {
+        userId,
+        pageTitle: 'New_Store'
+    })
 
+})
 
 
 

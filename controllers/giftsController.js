1e955a95a4aa095ae8a1e1ba911a817a6ab854cf
babylee2002 <babylee2002@gmail.com>
2017-12-31 00:33:01 -0500

    post route added

--- controllers/giftsController.js
+++ controllers/giftsController.js
@@ -19,9 +19,26 @@ router.get('/new', (req, res) => {
                 pageTitle: 'New_Gift'
             })
         })
-
 })
 
+router.post('/',(req,res)=>{
+    const userId = req.params.userId
+    const storeId = req.params.storeId
+
+    const newGift = req.body
+
+    User.findById(userId)
+    .then((user)=>{
+        const store = user.stores.storeId
+        store.giftToReturn.push(newGift)
+
+        return user.save()
+    })
+    .then(()=>{
+        res.redirect(`/users/${userId}/stores/${storeId}`)
+        
+    })
+})
 
 
 
